d4c708b838c896bfb7b7512465e2712e08c5e502
Mauricio <mauriciocarbajali@gmail.com>
2017-03-10 05:27:09 -0300

    modules...

--- build/main.bundle.js
+++ build/main.bundle.js
@@ -63,7 +63,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -73,22 +73,19 @@
 "use strict";
 
 
-//let (block scoped) instead of var (function scoped)
-
-
-//var calculateMonthlyPayment = function(principal, years, rate) {
-//let calculateMonthlyPayment = function(principal, years, rate) {
-var calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var calculateMonthlyPayment = exports.calculateMonthlyPayment = function calculateMonthlyPayment(principal, years, rate) {
     var monthlyRate = 0;
     if (rate) {
         monthlyRate = rate / 100 / 12;
     }
     var monthlyPayment = principal * monthlyRate / (1 - Math.pow(1 / (1 + monthlyRate), years * 12));
-
     return { principal: principal, years: years, rate: rate, monthlyPayment: monthlyPayment, monthlyRate: monthlyRate };
 };
 
-var calculateAmortization = function calculateAmortization(principal, years, rate) {
+var calculateAmortization = exports.calculateAmortization = function calculateAmortization(principal, years, rate) {
     var _calculateMonthlyPaym = calculateMonthlyPayment(principal, years, rate),
         monthlyRate = _calculateMonthlyPaym.monthlyRate,
         monthlyPayment = _calculateMonthlyPaym.monthlyPayment;
@@ -110,19 +107,29 @@ var calculateAmortization = function calculateAmortization(principal, years, rat
     return { monthlyPayment: monthlyPayment, monthlyRate: monthlyRate, amortization: amortization };
 };
 
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _mortgage = __webpack_require__(0);
+
+var mortgage = _interopRequireWildcard(_mortgage);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
 //document.getElementById('calcBtn').addEventListener('click', function () {
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
 
-    //destructing
-    //let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
-
-    var _calculateAmortizatio = calculateAmortization(principal, years, rate),
-        monthlyPayment = _calculateAmortizatio.monthlyPayment,
-        monthlyRate = _calculateAmortizatio.monthlyRate,
-        amortization = _calculateAmortizatio.amortization;
+    var _mortgage$calculateAm = mortgage.calculateAmortization(principal, years, rate),
+        monthlyPayment = _mortgage$calculateAm.monthlyPayment,
+        monthlyRate = _mortgage$calculateAm.monthlyRate,
+        amortization = _mortgage$calculateAm.amortization;
 
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
     document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
